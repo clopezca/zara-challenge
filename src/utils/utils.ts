8009e74de5c791e26bef7b2ae957e683ad48dbeb
Carolina Lopez Casanova <carolinalopezcasanova@gmail.com>
2026-04-17 14:23:30 +0200

    test: add unit tests for deduplicateById util

---
+++ src/utils/utils.ts
@@ -0,0 +1,9 @@
+export const deduplicateById = <T extends { id: string }>(items: T[]): T[] => {
+  const seen = new Set<string>()
+
+  return items.filter((item) => {
+    if (!item.id || seen.has(item.id)) return false
+    seen.add(item.id)
+    return true
+  })
+}
