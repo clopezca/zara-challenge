a488c1087b77bd99ca6c4497fdd1adbe7043c1c1
Carolina Lopez Casanova <carolinalopezcasanova@gmail.com>
2026-04-19 00:28:39 +0200

    feat: add ProductDetailPage with ProductImage, ProductInfo, StorageSelector and ColorSelector

--- src/pages/ProductDetailPage/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage/ProductDetailPage.tsx
@@ -2,19 +2,40 @@ import { useParams } from 'react-router-dom'
 import { useProductDetail } from '../../features/products/hooks/useProductDetail'
 
 import ErrorMessage from '../../components/ErrorMessage/ErrorMessage'
+import ProductImage from '../../features/products/components/ProductImage/ProductImage'
 
-//import styles from './ProductDetailPage.module.scss'
+import styles from './ProductDetailPage.module.scss'
+import ProductInfo from '../../features/products/components/ProductInfo/ProductInfo'
+import { useState } from 'react'
+import type { ColorOption, StorageOption } from '../../types/product.types'
 
 const ProductDetailPage = () => {
   const { id } = useParams<{ id: string }>()
   const { product, error } = useProductDetail(id!)
+  const [selectedColor, setSelectedColor] = useState<ColorOption | null>(null)
+  const [selectedStorage, setSelectedStorage] = useState<StorageOption | null>(null)
 
   if (error) return <ErrorMessage message={error} />
   if (!product) return null
 
+  const currentImage = selectedColor?.imageUrl ?? product.colorOptions[0]?.imageUrl
+
   return (
-    <main>
-      <div>{product.name}</div>
+    <main className={styles.page}>
+      <section className={styles.hero}>
+        <ProductImage imageUrl={currentImage} name={product.name} />
+        <ProductInfo
+          name={product.name}
+          basePrice={product.basePrice}
+          colorOptions={product.colorOptions}
+          storageOptions={product.storageOptions}
+          selectedColor={selectedColor}
+          selectedStorage={selectedStorage}
+          onColorSelect={setSelectedColor}
+          onStorageSelect={setSelectedStorage}
+          onAddToCart={() => console.log('add to cart')}
+        />
+      </section>
     </main>
   )
 }
