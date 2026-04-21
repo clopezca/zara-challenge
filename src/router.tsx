e471ca43255434910cfe7aa3755d976ef183f0e1
Carolina Lopez Casanova <carolinalopezcasanova@gmail.com>
2026-04-21 10:40:22 +0200

    refactor: wrap Routes in Suspense for lazy loading of components

--- src/router.tsx
+++ src/router.tsx
@@ -1,18 +1,22 @@
 import { Route, Routes } from 'react-router-dom'
 import { ROUTES } from './routes'
-import ProductListPage from './pages/ProductListPage/ProductListPage'
-import ProductDetailPage from './pages/ProductDetailPage/ProductDetailPage'
-import CartPage from './pages/CartPage/CartPage'
-import NotFoundPage from './pages/NotFoundPage/NotFoundPage'
+import { lazy, Suspense } from 'react'
+
+const ProductListPage = lazy(() => import('./pages/ProductListPage/ProductListPage'))
+const ProductDetailPage = lazy(() => import('./pages/ProductDetailPage/ProductDetailPage'))
+const CartPage = lazy(() => import('./pages/CartPage/CartPage'))
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'))
 
 const AppRouter = () => {
   return (
-    <Routes>
-      <Route path={ROUTES.HOME} element={<ProductListPage />} />
-      <Route path={ROUTES.PHONE_DETAIL} element={<ProductDetailPage />} />
-      <Route path={ROUTES.CART} element={<CartPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+    <Suspense fallback={null}>
+      <Routes>
+        <Route path={ROUTES.HOME} element={<ProductListPage />} />
+        <Route path={ROUTES.PHONE_DETAIL} element={<ProductDetailPage />} />
+        <Route path={ROUTES.CART} element={<CartPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Suspense>
   )
 }
 
